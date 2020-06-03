f283bae8a85f4548ad90fea0509403a1d3e45a3e
Swung0x48 <swung0x48@outlook.com>
2020-06-03 17:57:14 +0800

    Finished book

--- book.h
+++ book.h
@@ -5,6 +5,8 @@
 #ifndef BOOKSTORE_BOOK_H
 #define BOOKSTORE_BOOK_H
 #include <string>
+#include "Util.h"
+
 using namespace std;
 
 class book
@@ -17,11 +19,11 @@ private:
     double _price;
 public:
     book(
-            string name,
-            string author,
-            string pubHouse,
-            string Isbn,
-            double price
+            const string& name,
+            const string& author,
+            const string& pubHouse,
+            const string& Isbn,
+            const double& price
          ):
             _name(name),
             _author(author),
@@ -33,11 +35,28 @@ public:
     string getAuthor() { return _author; }
     string getPubHouse() { return _pubHouse; }
     string getIsbn() { return _isbn; }
-    double getPrice() { return _price; }
+    double getPrice() const { return _price; }
 
     bool operator< (const book& rhs) const  // overload operator< to make std::map happy.
     {
         return this->_name < rhs._name;
+    }
+
+    string toString(int para)   // 0 to stdout, 1 to file
+    {
+        if (para == 1)
+        {
+            return _name + " " + _author + " " + _pubHouse + " " + _isbn + " " + Util::to_string(_price) + " " + "\n";
+        }
+        else if (para == 0)
+        {
+            return
+                "书名: " + _name + "\n" +
+                "作者: " + _author + "\n" +
+                "出版社： " + _pubHouse + "\n" +
+                "ISBN: " + _isbn + "\n" +
+                "价格： " + Util::to_string(_price) + "\n";
+        }
     }
 };
 
