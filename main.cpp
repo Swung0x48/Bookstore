1ce11184786e635386d390ae1d2fecb1b60d992d
Swung0x48 <swung0x48@outlook.com>
2020-06-08 18:17:18 +0800

    Cleanup

--- main.cpp
+++ main.cpp
@@ -8,25 +8,6 @@ using namespace std;
 
 int main()
 {
-//    book mem("name", "Fred", "a pubhouse", "a-vvv-ccc", 50);
-//    book mem2(mem);
-//    book mem3(mem);
-//    book mem4("aaa", "Frd", "a puhouse", "a-vvv-ddc", 40);
-//    Vars::bookList.insert(mem);
-//    Vars::bookList.insert(mem2);
-//    Vars::bookList.insert(mem3);
-//    Vars::bookList.insert(mem4);
-//
-//    order o(me);
-//    o.addItem(mem, 1);
-//    o.addItem(mem3, 1);
-//    o.addItem(mem4, 1);
-//    Vars::orderList.push_back(o);
-//
-//    cout << o.getBuyer().getName() << endl;
-//    cout << o.getSubtotal() << endl;
-//    cout << Presenter::translate(true, o) << endl;
-
     File::openBookList();
     File::openMemberList();
 
